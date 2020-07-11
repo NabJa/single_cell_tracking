9fc132ea6da0b7207d39e10591d85ed04bddb36d
ge72feh <n.jabareen@tum.de>
2020-07-11 13:50:21 +0200

    Add resnet23 TFRecord data

--- src/mlt_detection/data/dataset.py
+++ src/mlt_detection/data/dataset.py
@@ -10,13 +10,16 @@ main_path
     -> ...
 """
 
+import pathlib
 from pathlib import Path
 import cv2
 import numpy as np
+import tensorflow as tf
 
 
 class Dataset:
     """Dataset class used to train ResNet23"""
+
     def __init__(self, main_path, pmap_name="prob_map.tif", img_name="image.tif"):
         """
         :param main_path: (str|Path) Path to main directory containing images (e.g. ./data/lensfree/).
@@ -28,7 +31,7 @@ class Dataset:
         self.images = np.array(list(self.main_path.rglob(img_name)))
         self.len_images = len(self.images)
         self.len_prob_maps = len(self.prob_maps)
-        assert self.len_prob_maps == self.len_images,\
+        assert self.len_prob_maps == self.len_images, \
             f"Number of probability maps {self.len_prob_maps} does not match number of images {self.len_images}"
         self.val_split = None
         self.train_ids = None
@@ -49,7 +52,7 @@ class Dataset:
     def generate_train_data(self, batch_size=1):
         """Data generator on training data."""
         # TODO add augmentation
-        assert (self.train_images is not None) and (self.train_prob_maps is not None),\
+        assert (self.train_images is not None) and (self.train_prob_maps is not None), \
             "No training data found. Split dataset before generating training data."
 
         images, pmaps = [], []
@@ -90,7 +93,7 @@ class Dataset:
         self.val_split = val_split
         ids = list(range(self.__len__()))
         if type(self.val_split) is float:
-            assert 0 < self.val_split < 1,\
+            assert 0 < self.val_split < 1, \
                 f"Invalid validation split size. Must be: 0 < val_split < 1. Given: {self.val_split} "
             number_of_val_images = int(self.val_split * self.len_images)
             val_ids = np.random.choice(list(range(self.len_images)), number_of_val_images, replace=False)
@@ -106,3 +109,53 @@ class Dataset:
         self.train_ids = train_ids
         self.val_ids = val_ids
         return train_ids, val_ids
+
+
+class TFDataset:
+    """TFRecordDataset for ResNet23."""
+
+    def __init__(self, records, batch_size=1):
+        """
+        :param records: Path to TF Record file(s). Might be a list.
+        """
+        self.batch_size = batch_size
+        self._parse_records(records)
+        self._create_data()
+
+    def _parse_records(self, records):
+        if isinstance(records, list):
+            self.records = [str(x) for x in records]
+        elif isinstance(records, pathlib.PurePath):
+            self.records = str(records)
+        elif isinstance(records, str):
+            self.records = records
+        else:
+            raise TypeError(f"Invalid record type: {type(records)}")
+
+    @staticmethod
+    def _parse_image_function(example_proto):
+
+        image_feature_description = {
+            'image/filename': tf.io.FixedLenFeature([], tf.string),
+            'image/encoded': tf.io.FixedLenFeature([], tf.string),
+            'map/filename': tf.io.FixedLenFeature([], tf.string),
+            'map/encoded': tf.io.FixedLenFeature([], tf.string),
+        }
+
+        parsed_features = tf.io.parse_single_example(example_proto, image_feature_description)
+        prob_map = tf.image.decode_png(parsed_features["map/encoded"], channels=1, dtype=tf.float16, name="prob_map")
+        image = tf.image.decode_png(parsed_features['image/encoded'], channels=3, dtype=tf.uint8, name="image")
+        return image, prob_map
+
+    def _create_data(self):
+        dataset = tf.data.TFRecordDataset(self.records)
+        dataset = dataset.map(self._parse_image_function)
+        dataset = dataset.repeat()
+        dataset = dataset.shuffle(1000)
+        dataset = dataset.take(self.batch_size)
+        self.data = dataset
+
+
+path = Path(r"D:\Nabil_object_detection\data\resnet23_data\lensfree\nrk_experiment\resnet_data.tfrecord")
+data = TFDataset(path)
+
--- src/mlt_detection/data/tfrecord_generation.py
+++ src/mlt_detection/data/tfrecord_generation.py
@@ -10,26 +10,74 @@ import tensorflow as tf
 from src.utils import bbox_utils as bu
 
 
-def record_from_resnet_data(inp, out=None, ignore=None):
+def object_detection_record_from_resnet_data(inp, out=None, ignore=None):
     inp = Path(inp)
     if out is None:
         out = str(inp/"resnet_data.tfrecord")
 
     print(f"Collecting data. Ignoring: {ignore} ...")
-    coord_path = [x for x in inp.rglob("coordinates.npy") if len(set(ignore).intersection(set(x.parts))) < 1]
-    img_path = [x for x in inp.rglob("image.tif") if len(set(ignore).intersection(set(x.parts))) < 1]
+    if ignore is None:
+        coord_path = [x for x in inp.rglob("coordinates.npy")]
+        img_path = [x for x in inp.rglob("image.tif")]
+    else:
+        coord_path = [x for x in inp.rglob("coordinates.npy") if len(set(ignore).intersection(set(x.parts))) < 1]
+        img_path = [x for x in inp.rglob("image.tif") if len(set(ignore).intersection(set(x.parts))) < 1]
 
     coords = [np.load(c) for c in coord_path]
     bboxes = [np.array([bu.point_to_box(p, 40, img_shape=(224, 224)) for p in points]) for points in coords]
 
     print(f"Writing record ...")
-    write_tf_record(img_path, bboxes, out)
+    write_detection_record(img_path, bboxes, out)
     print("FINISHED")
 
 
-def write_tf_record(images, bboxes, filename):
+def image_pair_record_from_resnet_data(inp, image_name="image.tif", map_name="prob_map.tif", out=None, ignore=None):
     """
-    Write tf record to filename.
+    Generate image pairs (microscopy image, probability map).
+
+    :param inp: Root dir to search data from.
+    :param image_name: Naming convention for microscopy images.
+    :param map_name: Naming convention for probability map.
+    :param out: Output path.
+    :param ignore: List of folders to be ignored.
+    """
+    inp = Path(inp)
+    if out is None:
+        out = str(inp/"resnet_data.tfrecord")
+
+    print(f"Collecting data. Ignoring: {ignore} ...")
+    if ignore is None:
+        img_path = [x for x in inp.rglob("image.tif")]
+        pm_path = [x for x in inp.rglob("prob_map.tif")]
+    else:
+        img_path = [x for x in inp.rglob(image_name) if len(set(ignore).intersection(set(x.parts))) < 1]
+        pm_path = [x for x in inp.rglob(map_name) if len(set(ignore).intersection(set(x.parts))) < 1]
+
+    print(f"Writing record ...")
+    write_pair_record(img_path, pm_path, out)
+    print("FINISHED")
+
+
+def write_pair_record(images, maps, filename):
+    """
+    Write resnet23 image pairs tf record to filename.
+
+    :param images: List of image paths
+    :param maps: List of probability map paths
+    :param filename: Name of tf record file
+    :return: None
+    """
+
+    with tf.io.TFRecordWriter(str(filename)) as writer:
+        for i, (image_path, pm_path) in enumerate(zip(images, maps)):
+            tf_example = pair_to_tf_example(image_path, pm_path)
+            writer.write(tf_example.SerializeToString())
+    print("Created tf-record file in: \n", filename)
+
+
+def write_detection_record(images, bboxes, filename):
+    """
+    Write object detection record to filename.
 
     :param images: List of image paths
     :param bboxes: List of bboxes in format (xmin, ymin, xmax, ymax)
@@ -37,13 +85,44 @@ def write_tf_record(images, bboxes, filename):
     :return: None
     """
 
-    with tf.io.TFRecordWriter(filename) as writer:
+    with tf.io.TFRecordWriter(str(filename)) as writer:
 
         for i, (image_path, bbox) in enumerate(zip(images, bboxes)):
             image = cv2.imread(str(image_path), cv2.IMREAD_UNCHANGED)
             tf_example = bbox_to_tf_example(image, image_path, bbox)
             writer.write(tf_example.SerializeToString())
     print("Created tf-record file in: \n", filename)
+
+
+def encode_to_png(img):
+    img = tf.cast(img, tf.uint8)
+    if len(img.shape) == 2:
+        return tf.image.encode_png(tf.expand_dims(img, axis=-1))
+    elif len(img.shape) == 3:
+        return tf.image.encode_png(img)
+    else:
+        raise ValueError(f"Invalid image shape {img.shape} in {img}")
+
+
+def pair_to_tf_example(image_path, map_path):
+
+    image_path, map_path = str(image_path), str(map_path)
+
+    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    map_image = cv2.imread(map_path, cv2.IMREAD_UNCHANGED)
+
+    # TODO probability map needs different encoding, supporting floating points
+    ecoded_image = encode_to_png(image)
+    encoded_map = encode_to_png(map_image)
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/filename': _bytes_feature(image_path.encode("unicode_escape")),
+        'image/encoded': _bytes_feature(ecoded_image),
+        'map/filename': _bytes_feature(map_path.encode("unicode_escape")),
+        'map/encoded': _bytes_feature(encoded_map),
+    }))
+
+    return tf_example
 
 
 def bbox_to_tf_example(image, filename, bboxes):
@@ -53,7 +132,7 @@ def bbox_to_tf_example(image, filename, bboxes):
     image_ext = ".png"
     filename = str(filename)
 
-    encoded_image = cv2.imencode(image_ext, image)[1].tostring()
+    encoded_image = encode_to_png(image)
 
     # Parse bounding boxes
     xmins, xmaxs, ymins, ymaxs = [], [], [], []
@@ -69,39 +148,42 @@ def bbox_to_tf_example(image, filename, bboxes):
     classes = [1]
 
     tf_example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': int64_feature(height),
-        'image/width': int64_feature(width),
-        'image/filename': bytes_feature(filename.encode("unicode_escape")),
-        'image/source_id': bytes_feature(filename.encode("unicode_escape")),
-        'image/encoded': bytes_feature(encoded_image),
-        'image/format': bytes_feature(image_ext.encode("unicode_escape")),
-        'image/object/bbox/xmin': float_list_feature(xmins),
-        'image/object/bbox/xmax': float_list_feature(xmaxs),
-        'image/object/bbox/ymin': float_list_feature(ymins),
-        'image/object/bbox/ymax': float_list_feature(ymaxs),
-        'image/object/class/text': bytes_list_feature(classes_text),
-        'image/object/class/label': int64_list_feature(classes),
+        'image/height': _int64_feature(height),
+        'image/width': _int64_feature(width),
+        'image/filename': _bytes_feature(filename.encode("unicode_escape")),
+        'image/source_id': _bytes_feature(filename.encode("unicode_escape")),
+        'image/encoded': _bytes_feature(encoded_image),
+        'image/format': _bytes_feature(image_ext.encode("unicode_escape")),
+        'image/object/bbox/xmin': _float_list_feature(xmins),
+        'image/object/bbox/xmax': _float_list_feature(xmaxs),
+        'image/object/bbox/ymin': _float_list_feature(ymins),
+        'image/object/bbox/ymax': _float_list_feature(ymaxs),
+        'image/object/class/text': _bytes_list_feature(classes_text),
+        'image/object/class/label': _int64_list_feature(classes),
     }))
     return tf_example
 
 
-def int64_feature(value):
+def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 
-def int64_list_feature(value):
+def _int64_list_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
 
-def bytes_feature(value):
+def _bytes_feature(value):
+    """Returns a bytes_list from a string / byte."""
+    if isinstance(value, type(tf.constant(0))):
+        value = value.numpy()  # BytesList won't unpack a string from an EagerTensor.
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
-def bytes_list_feature(value):
+def _bytes_list_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
 
 
-def float_list_feature(value):
+def _float_list_feature(value):
     return tf.train.Feature(float_list=tf.train.FloatList(value=value))
 
 
@@ -113,4 +195,4 @@ if __name__ == '__main__':
                                                     "e.g. --ignore nrk_experiment a549_experiment")
     args = parser.parse_args()
 
-    record_from_resnet_data(args.input, args.output, args.ignore)
+    image_pair_record_from_resnet_data(args.input, args.output, args.ignore)
